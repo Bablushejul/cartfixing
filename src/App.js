2727cad9269e1760aeb97d843ceeb0a2177c7552
Bablushejul <shejulbablu@gmail.com>
2023-05-17 02:10:22 +0530

    final comit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Product from './components/Shop/Product';
-import { useEffect } from 'react';
+import Notification from './components/UI/Notification'
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+
 
 function App() {
+
+  const dispatch=useDispatch()
 
 const showCart=useSelector((state)=>!state.ui.cartIsVisible)
 
 const cart = useSelector((state)=>state.cart)
 
+const notification=useSelector(state=>state.ui.notification)
+
 useEffect(()=>{
-  fetch("https://cartfixing-default-rtdb.firebaseio.com/cart.json",{
+
+const sendCartData=async()=>{
+
+  dispatch(uiActions.showNotification({
+    status:'pending',
+    title:'Sending...',
+    message:'Sending data to cart'
+  }))
+
+ const response =await fetch("https://cartfixing-default-rtdb.firebaseio.com/cart.json",{
     method:'PUT',
     body:JSON.stringify(cart)
   })
-},[cart])
+
+  if(!response.ok){
+
+    throw new Error('Sending cart data is failed')
+  }
+
+
+  dispatch(uiActions.showNotification({
+    status:'succes',
+    title:'Success',
+    message:'Sent data to cart Successfully'
+  }))
+
+}
+
+
+sendCartData().catch((error)=>{
+  dispatch(uiActions.showNotification({
+    status:'error',
+    title:'Error',
+    message:'Sending cart data is failed'
+  }))
+
+})
+  
+},[cart,dispatch])
 
   return (
+    <Fragment>
+     {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />} 
     <Layout>
       {showCart&&<Cart />}
       <Product />
     </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,18 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
-
-
-const uiSlice=createSlice({
-    name:'ul',
-    initialState:{ cartIsVisible:false },
-    reducers:{
-        toggle(state){
-            state.cartIsVisible=!state.cartIsVisible;
-        }
-    }
-})
-
-export const uiActions=uiSlice.actions;
+export const uiActions = uiSlice.actions;
 
 export default uiSlice;
