fc915629dd2c221d691a0b1f793adba1832aed18
Bablushejul <shejulbablu@gmail.com>
2023-05-17 01:20:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Product from './components/Shop/Product';
+import { useEffect } from 'react';
 
 function App() {
 
 const showCart=useSelector((state)=>!state.ui.cartIsVisible)
+
+const cart = useSelector((state)=>state.cart)
+
+useEffect(()=>{
+  fetch("https://cartfixing-default-rtdb.firebaseio.com/cart.json",{
+    method:'PUT',
+    body:JSON.stringify(cart)
+  })
+},[cart])
 
   return (
     <Layout>
